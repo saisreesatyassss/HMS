e3d262f5401257123c74ecd4cb95f21adf91cf23
saisreesatyassss <saisreesatyassss@gmail.com>
2024-04-17 10:33:16 +0530

    init

--- components/Features/featuresData.tsx
+++ components/Features/featuresData.tsx
@@ -6,43 +6,44 @@ const featuresData: Feature[] = [
     icon: "/images/icon/icon-01.svg",
     title: "AI chatbot",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+      "Engage users with an intelligent conversational agent powered by artificial intelligence, providing instant assistance and answering queries 24/7.",
   },
   {
     id: 2,
     icon: "/images/icon/icon-02.svg",
     title: "Symptom analysis",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+      "Empower users to assess their health conditions by inputting symptoms, receiving accurate analysis, and guidance on potential next steps.",
   },
   {
     id: 3,
     icon: "/images/icon/icon-03.svg",
     title: "Diet planner",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+      "Help users achieve their health and fitness goals with personalized diet plans tailored to their nutritional needs and preferences.",
   },
   {
     id: 4,
     icon: "/images/icon/icon-04.svg",
     title: "Doctor consultation",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+      "Connect users with qualified healthcare professionals for virtual consultations, diagnosis, treatment plans, and medical advice from the comfort of their homes.",
   },
   {
     id: 5,
     icon: "/images/icon/icon-05.svg",
     title: "Data privacy",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+      "Ensure the confidentiality and security of users' sensitive health data through robust privacy measures and compliance with data protection regulations.",
   },
   {
     id: 6,
     icon: "/images/icon/icon-06.svg",
     title: "Facial recognition",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+      "Enhance user experience and security with facial recognition technology for identity verification, access control, and personalized interactions.",
   },
 ];
+
 
 export default featuresData;
