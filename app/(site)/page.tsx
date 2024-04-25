faef7103a4a690f641890e4d76f3d5c7fdad9af0
saisreesatyassss <saisreesatyassss@gmail.com>
2024-04-25 20:47:02 +0530

    chnages

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -14,7 +14,7 @@ import Blog from "@/components/Blog";
 import Testimonial from "@/components/Testimonial";
 
 export const metadata: Metadata = {
-  title: "Next.js Starter Template for SaaS Startups  ",
+  title: "HMS",
   description: "This is Home ",
   // other metadata
 };
